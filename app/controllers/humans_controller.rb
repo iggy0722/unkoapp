cb2e79ec3db5ac4d9eacb6af927a5b222ae26fd9
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-12-06 02:17:25 +0900

    コントローラー追記

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,12 +1,16 @@
 class HumansController < ApplicationController
-  
+
   def index
+    @humans = Human.all
   end
 
   def new
+    @human = Human.new
   end
 
   def create
+    Human.create(human_parameter)
+    redirect_to root_path
   end
 
 end
