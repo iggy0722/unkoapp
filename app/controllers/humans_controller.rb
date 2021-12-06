d63fc977def9f3e14997f6fa32559a6ae37d3e86
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-12-06 15:59:45 +0900

    子ユーザー編集機能

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,16 +1,15 @@
 class HumansController < ApplicationController
-
+  before_action :set_human, only: [:show , :edit , :update]
   def index
     @humans = Human.all
   end
 
   def new
     @human = Human.new
-    binding.pry
   end
 
   def create
-    @human = Human.create(human_params)
+    @human = Human.create(human_parameter)
     if @human.save
       redirect_to root_path
     else
@@ -19,13 +18,30 @@ class HumansController < ApplicationController
   end
 
   def show
-    @human = Human.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @human.update(human_params)
+      redirect_to human_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
   end
 
   private
+  def human_parameter
+    params.permit(:name,:type_id).merge(user_id: current_user.id)
+  end
 
   def human_params
-    params.permit(:name,:type_id).merge(user_id: current_user.id)
+    params.require(:human).permit(:name,:type_id).merge(user_id: current_user.id)
+  end
+
+  def set_human
+    @human = Human.find(params[:id])
   end
 
 end
