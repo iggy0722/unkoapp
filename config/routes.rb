b1991bd7ab51ca7df8a9fedeecf95852c8a8db5c
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-12-07 16:58:48 +0900

    ルーティングのネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "humans#index"
-  resources :humans
+  resources :humans do
+    resources :poops, only:[:index, :create]
+  end
 end
