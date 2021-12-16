31f841a74ddd763559ad90145bc80b59d2814186
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-12-16 14:57:35 +0900

    画像をアップロード

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,9 +4,10 @@ class State < ActiveHash::Base
     { id: 2, name: '普通便' },
     { id: 3, name: 'コロコロ便' },
     { id: 4, name: 'カチカチ便' },
-    { id: 5, name: 'ややカチ便' },
+    { id: 5, name: 'ちょいカタ便' },
     { id: 6, name: 'ドロドロ便' },
-    { id: 7, name: 'びちゃびちゃ' }
+    { id: 7, name: 'びちゃびちゃ' },
+    { id: 8, name: '不消化便' }
   ]
   include ActiveHash::Associations
   has_many :poops
