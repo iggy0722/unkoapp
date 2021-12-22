d443cb5b06fa6aa3ae8aaf11eecaa4deeea1a52c
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-12-22 16:27:12 +0900

    backカラー変更

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,8 +1,13 @@
 class Type < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '人間（大人）' },
-    { id: 3, name: '人間（子供）' }
+    { id: 2, name: '人間（成体）' },
+    { id: 3, name: '人間（幼体）' },
+    { id: 4, name: '犬' },
+    { id: 5, name: '猫' },
+    { id: 6, name: '爬虫類' },
+    { id: 7, name: '鳥' },
+    { id: 8, name: '魚' }
   ]
   include ActiveHash::Associations
   has_many :humans
